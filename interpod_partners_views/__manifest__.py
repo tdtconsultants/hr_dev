f10a879b6b9293a801d0a7d79f9af4ae1fbf51af
FernandoAL <feraylv@hotmail.com>
2017-11-03 12:44:33 -0300

    renamed partners modules

--- interpod_partners_views/__manifest__.py
+++ interpod_partners_views/__manifest__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "interpod_partners",
+    'name': "interpod_partners_views",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        """,
 
     'description': """
         Long description of module's purpose
