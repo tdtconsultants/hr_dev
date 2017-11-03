4370c8f2848c4f498667449793eacb79837956da
FernandoAL <feraylv@hotmail.com>
2017-11-03 14:19:06 -0300

    updated products attributes

--- interpod_product_attributes/models/product_template.py
+++ interpod_product_attributes/models/product_template.py
@@ -6,3 +6,8 @@ class Product(models.Model):
     #name = fields.Char('Name', index=True, required=True)
     interpod_product_projects = fields.Many2many('project.project', string = "Project(s)", relation = 'product_template_project_project_rel' ,required = False, translate = False)
 
+
+class ProductProduct(models.Model):
+
+    _inherit = "product.product"
+    interpod_product_projects = fields.Many2many('project.project', string = "Project", related = 'product_tmpl_id.interpod_product_projects', required = False, store = False, translate = False)
