10ca848dcf0e1be4b339d075a23988f0783babe5
FernandoAL <feraylv@hotmail.com>
2017-11-16 11:19:43 -0300

    quality alert product category

--- interpod_quality_alerts_attributes/models/quality_alert.py
+++ interpod_quality_alerts_attributes/models/quality_alert.py
@@ -5,7 +5,7 @@ class QualityAlert(models.Model):
 
     #name = fields.Char('Name', index=True, required=True)
     interpod_related_iso_clause = fields.Char("Related ISO Clause", required = False, translate = False)
-
+    interpod_product_category = fields.Many2one("product.category", "Product Category")
     interpod_quality_alert_product_category = fields.Many2one('product.category' , string = "Stage",required = False,  translate = False)
     interpod_ncr_title = fields.Char('Title', required = False, translate = False)
     interpod_linked_documents = fields.Char('Linked Documents', required = False, translate = False)
