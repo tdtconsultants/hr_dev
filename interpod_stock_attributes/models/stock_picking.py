55acda15540d0874c6c81ab53ba78bf64398766f
Bruno Figares <bfigares@tdtconsultants.com>
2017-12-21 16:42:45 -0300

    stock locations order

--- interpod_stock_attributes/models/stock_picking.py
+++ interpod_stock_attributes/models/stock_picking.py
@@ -5,3 +5,21 @@ from odoo import models, fields, api
 class interpod_stock_attributes(models.Model):
      _inherit = 'stock.picking'
      is_locked = fields.Boolean(default=False, help='When the picking is not done this allows changing the initial demand. When the picking is done this allows changing the done quantities.')
+
+
+
+class Location(models.Model):
+    _inherit = "stock.location"
+    _order = "parent_left, name"
+
+    complete_name = fields.Char("Full Location Name", compute='_compute_complete_name', store=True, index=True)
+    name = fields.Char('Location Name', required=True, translate=True, index=True)
+
+
+
+
+class InventoryLine(models.Model):
+    _inherit = "stock.inventory.line"
+    _order = "location_name"
+    location_name = fields.Char('Location Name', related='location_id.complete_name', store=True, index = True)
+
