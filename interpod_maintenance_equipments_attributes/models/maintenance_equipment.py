8b2ae9c55f4a970eec121197975286ffed7d2834
FernandoAyL <fernandoayl@gmail.com>
2018-04-03 19:59:57 +0000

    maintenance equipments fix

--- interpod_maintenance_equipments_attributes/models/maintenance_equipment.py
+++ interpod_maintenance_equipments_attributes/models/maintenance_equipment.py
@@ -5,6 +5,8 @@ class MaintenanceEquipment(models.Model):
 
     interpod_asset_number = fields.Char("Asset Number", required = False, translate = False)
 
+
+    @api.one
     def _compute_display_name(self):
         self.display_name = ('[' + self.interpod_asset_number +'] ' + self.name) if self.interpod_asset_number else self.name
 
