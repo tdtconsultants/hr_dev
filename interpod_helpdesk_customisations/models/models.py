e48891dfe48c612ea2dc148f698f56250995f319
FernandoAyL <fernandoayl@gmail.com>
2018-04-05 14:34:12 +0000

    Helpdesk changes, filters on Assigned on, Customer, and added visibility of the tickets to the followers and the customer

--- interpod_helpdesk_customisations/models/models.py
+++ interpod_helpdesk_customisations/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class HelpdeskTicket(models.Model):
@@ -8,8 +9,26 @@ class HelpdeskTicket(models.Model):
 
     user_id = fields.Many2one('res.users', string='Assigned to', track_visibility='onchange', domain=[])
 
+    partner_id = fields.Many2one('res.partner', string='Customer', domain=[('is_employee','=',True)])
+
+    team_id = fields.Many2one('helpdesk.team', string='Helpdesk Team', default=None, index=True, required=True)
+    true_field = fields.Boolean(default=True, readonly=True)
+
     @api.model
     def create(self, vals):
         if not vals.get('partner_id'):
             vals['partner_id'] = self.env.user.partner_id.id
         return super(HelpdeskTicket, self).create(vals)
+
+class User(models.Model):
+    _inherit = 'res.users'
+
+    @api.one
+    def _compute_is_helpdesk_user(self):
+        try:
+            group = self.env.ref('helpdesk.group_helpdesk_user')
+        except Exception:
+            raise UserError('External ID of group: "helpdesk.group_helpdesk_user" not found')
+        self.is_helpdesk_user = group.id in self.groups_id.ids
+
+    is_helpdesk_user = fields.Boolean(compute='_compute_is_helpdesk_user', store=True)
--- interpod_hr_employees_customisations/models/models.py
+++ interpod_hr_employees_customisations/models/models.py
@@ -2,9 +2,43 @@
 
 from odoo import models, fields, api
 
-class interpod_hr_employees_customisations(models.Model):
+class Employee(models.Model):
 
     _inherit = "hr.employee"
 
     personal_phone = fields.Char()
+    
+    @api.multi
+    def write(self, vals):
+        if 'related_partner_id' in vals:
+            partner = self.env['res.partner'].browse(vals['related_partner_id'])
+            partner._compute_is_employee()
+        if 'user_id' in vals:
+            partner = self.env['res.users'].browse(vals['user_id']).partner_id
+            partner._compute_is_employee()
+        return super(Employee, self).write(vals)
+    
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    related_partner_id = fields.Many2one('res.partner', compute='_compute_related_partner', store=True)
+
+
+class Partner(models.Model):
+
+    _inherit = "res.partner"
+
+    @api.one
+    def _compute_is_employee(self):
+        if self.employee:
+            return True
+        employee = self.env['hr.employee'].search([('related_partner_id','=', self.id)])
+        self.is_employee = True if employee else False
+        
+    is_employee = fields.Boolean(compute="_compute_is_employee", store=True)
+
+    def compute_all_is_employee(self):
+        partners = self.env['res.partner'].search([])
+        partners.mapped(lambda p: p._compute_is_employee())
 
