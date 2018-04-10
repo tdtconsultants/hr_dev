ac1bf53ec4b105889bebb5a03b48fe4879f8ab96
FernandoAyL <fernandoayl@gmail.com>
2018-04-10 12:48:09 -0300

    tdt_project_manager_financials: Implement on top of analytic accounts
    And update the manifest.
    
    [ismael@tdt: edited message and contents]

--- tdt_project_manager_financials/__manifest__.py
+++ tdt_project_manager_financials/__manifest__.py
@@ -6,24 +6,22 @@
         Allow Project Manager to have access to specified accounts.
         """,
 
-    'description': """
-    """,
-
     'author': "TDT Consultants",
     'website': "http://www.tdtconsultants.com",
 
     # Categories can be used to filter modules in modules listing
     # for the full list
-    'category': 'Accounting',
+    'category': 'tdt',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account'],
+    'depends': ['base','account', 'project','interpod_general_permissions','hr_timesheet','hr_expense','analytic','hr_holidays'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'security/ir.rule.xml',
-        'views/account.xml',
+        'views/views.xml',
+        'views/hide_analytic_accounts.xml',
     ],
+    'license': 'OPL-1',
 }
