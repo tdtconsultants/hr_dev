ec99fa56e3dbe5ebd69f671af674520c8976e58c
Ismael Luceno <ismael@iodev.co.uk>
2018-06-27 11:34:36 -0300

    interpod_payment_receipt_report: Show credit applied in remittance advices

--- interpod_payment_receipt_report/__manifest__.py
+++ interpod_payment_receipt_report/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['web_enterprise'],
+    'depends': ['web_enterprise', 'account'],
 
     # always loaded
     'data': [
--- interpod_payment_receipt_report/models/models.py
+++ interpod_payment_receipt_report/models/models.py
@@ -1,15 +1,57 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.tools import float_is_zero
 
-# class interpod_payment_receipt_report(models.Model):
-#     _name = 'interpod_payment_receipt_report.interpod_payment_receipt_report'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    credit_applied = fields.Monetary(
+        string="Credit Applied",
+        help="Credit applied to the invoice",
+        compute='_get_credit_applied')
+    payment = fields.Monetary(
+        string="Payment",
+        help="Payment, not counting credit notes",
+        compute='_get_payment')
+
+    @api.one
+    @api.depends('credit_applied')
+    def _get_payment(self):
+        """Compute the amount paid w/o credit notes"""
+        self.payment = self.amount_total - self.credit_applied - self.residual
+
+    @api.one
+    def _get_credit_applied(self):
+        """Compute the amount of credit applied"""
+        payments = self._get_payments()
+        self.credit_applied = sum(
+            i['amount']
+            for i in payments
+            if i['payment_id'].journal_id.type == 'purchase')
+
+    def _get_payments(self):
+        """Get payments associated with an invoice"""
+        payments = []
+        for payment in self.payment_move_line_ids:
+            if self.type in ('out_invoice', 'in_refund'):
+                movs, move_id = payment.matched_debit_ids, lambda p: p.debit_move_id
+            elif self.type in ('in_invoice', 'out_refund'):
+                movs, move_id = payment.matched_credit_ids, lambda p: p.credit_move_id
+            if movs and all(p.currency_id == self.currency_id for p in movs):
+                amount_currency = sum(p.amount_currency for p in movs if move_id(p) in self.move_id.line_ids)
+                amount_to_show = amount_currency
+            else:
+                amount = sum(p.amount for p in movs if move_id(p) in self.move_id.line_ids)
+                amount_to_show = payment.company_id \
+                    .currency_id.with_context(date=self.date) \
+                    .compute(amount, self.currency_id)
+            if float_is_zero(amount_to_show, precision_rounding=self.currency_id.rounding):
+                continue
+            payments.extend([{
+                'amount': amount_to_show,
+                'currency_id': self.currency_id,
+                'payment_id': payment,
+            }])
+        return payments
