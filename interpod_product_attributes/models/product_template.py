c84e1bbc3a53a2c1843e470eb4bf0c89a6d290f9
Ismael Luceno <ismael@iodev.co.uk>
2018-07-03 11:44:00 -0300

    interpod_product_attributes: Code cleanup

--- interpod_product_attributes/models/product_template.py
+++ interpod_product_attributes/models/product_template.py
@@ -3,11 +3,23 @@ from openerp import models, api, fields, _
 class Product(models.Model):
     _inherit = "product.template"
 
-    #name = fields.Char('Name', index=True, required=True)
-    interpod_product_projects = fields.Many2many('project.project', string = "Project(s)", relation = 'product_template_project_project_rel' ,required = False, translate = False)
+    interpod_product_projects = fields.Many2many(
+        'project.project',
+        string="Project(s)",
+        relation='product_template_project_project_rel',
+        required=False,
+        translate=False
+    )
 
 
 class ProductProduct(models.Model):
-
     _inherit = "product.product"
-    interpod_product_projects = fields.Many2many('project.project', string = "Project", related = 'product_tmpl_id.interpod_product_projects', required = False, store = False, translate = False)
+
+    interpod_product_projects = fields.Many2many(
+        'project.project',
+        string="Project",
+        related='product_tmpl_id.interpod_product_projects',
+        required=False,
+        store=False,
+        translate=False
+    )
