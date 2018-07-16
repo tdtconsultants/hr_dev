86d8a6d93a241533a606484bae58ea92cfb6f741
Ismael Luceno <ismael@iodev.co.uk>
2018-07-16 15:49:17 -0300

    interpod_hr_employees_customisations: Update dependencies

--- interpod_hr_employees_customisations/__manifest__.py
+++ interpod_hr_employees_customisations/__manifest__.py
@@ -17,7 +17,9 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['hr'],
+    'depends': [
+        'hr_contract',
+    ],
 
     # always loaded
     'data': [
